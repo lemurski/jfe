1dcb8d4a7d48cf51dd4b188cf12c05b1e635900a
lemurski <kacperlem@gmail.com>
2022-02-26 00:43:56 +0100

    comi

--- cookie/settings.py
+++ cookie/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--$%_u0rzlim(96ky6vk!h54__$jx!&5%!*u^eh0kf%m&ydm*v5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['portfolio-7cvacj2cra-ew.a.run.app','127.0.0.1','kacperlem.dev']
 
